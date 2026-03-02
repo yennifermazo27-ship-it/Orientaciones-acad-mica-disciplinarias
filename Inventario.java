a85d1d6da933ec1170d62a465c146b6e5366b934
yennifer mazo montoya <yennifermzo27@gmail.com>
2026-03-02 11:29:19 -0500

    feat-claseHija1

--- Inventario.java
+++ Inventario.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+
 public class Inventario {
+
+    private ArrayList<Producto> listaProductos;
+
+    public Inventario() {
+        listaProductos = new ArrayList<>();
+    }
+
     
+    public Producto buscarProducto(String codigo) {
+        for (Producto p : listaProductos) {
+            if (p.getCodigo().equalsIgnoreCase(codigo)) {
+                return p;
+            }
+        }
+        return null;
+    }
 }
