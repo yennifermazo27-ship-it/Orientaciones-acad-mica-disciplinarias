8e9d61e7bc32a0b75766ab853cc10c97a3175bf3
yennifer mazo montoya <yennifermzo27@gmail.com>
2026-03-02 10:08:02 -0500

    feat-clasePadre2

--- Producto.java
+++ Producto.java
@@ -11,4 +11,56 @@ public class Producto {
         this.precio = precio;
         this.stock = stock;
     }
+
+public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+   
+    public void setPrecio(double precio) {
+        if (precio >= 0) {
+            this.precio = precio;
+        }
+    }
+
+    
+    public void aumentarStock(int cantidad) {
+        if (cantidad > 0) {
+            stock += cantidad;
+        }
+    }
+
+    
+    public boolean disminuirStock(int cantidad) {
+        if (cantidad > 0 && cantidad <= stock) {
+            stock -= cantidad;
+            return true;
+        }
+        return false;
+    }
+
+    
+    public double calcularValorTotal() {
+        return precio * stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Código: " + codigo +
+                " | Nombre: " + nombre +
+                " | Precio: " + precio +
+                " | Stock: " + stock;
+    }
 }
