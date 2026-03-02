1be234521a60f4123a0d2edc2682372d1abdc68d
yennifer mazo montoya <yennifermzo27@gmail.com>
2026-03-02 09:57:40 -0500

    feat-clasePadre1

--- Producto.java
+++ Producto.java
@@ -1,3 +1,14 @@
 public class Producto {
-    
+
+    private String codigo;
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    public Producto(String codigo, String nombre, double precio, int stock) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
 }
